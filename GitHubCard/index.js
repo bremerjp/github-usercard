c2db2a7f2c35f4da941adec2cd062a8ce1b5384c
Jacob Bremer <bremerjp@gmail.com>
2019-12-05 19:58:56 -0500

    stretch  - dynamically created follower list

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,26 +1,45 @@
 const cards = document.querySelector('.cards');
-const followersArray = ["eralpkor","tetondan", "dustinmyers", "justsml", "luishrd", "bigknell"];
 
-axios.get("https://api.github.com/users/bremerjp").then(response => {
-  console.log(response)  
-  const newCard = createCard(response.data);
-    cards.appendChild(newCard);
+axios
+  .get("https://api.github.com/users/bremerjp")
+  .then(response => {
+    cards.appendChild(createCard(response.data));
   })
+
   .catch(error => {
     console.log("The data was not returned", error);
   });
+
+const followersArray = ["tetondan", "dustinmyers", "justsml", "luishrd", "bigknell"];
 
 followersArray.forEach(element => {
-  const userName = element;
-  axios.get(`https://api.github.com/users/${element}`).then(response => {
-  console.log(response)  
-  const newCard = createCard(response.data);
-    cards.appendChild(newCard);
+  axios
+    .get(`https://api.github.com/users/${element}`)
+    .then(response => {
+      cards.appendChild(createCard(response.data));
+    })
+    .catch(error => {
+      console.log("The data was not returned", error);
+    });
+});
+///stretch  - dynamically created follower list
+axios
+  .get('https://api.github.com/users/bremerjp/followers')
+  .then(response => {
+    return response.data.map(u => u.url);
+  })
+  .then(response => {
+    response.forEach(i => {
+      axios.get(i)
+        .then(response => {
+          document.querySelector('.cards').appendChild(createCard(response.data));
+        });
+    });
   })
   .catch(error => {
-    console.log("The data was not returned", error);
+    console.log('Data was not returned', error);
   });
-});
+
 
 const createCard = (userObj) => {
   const card = document.createElement('div'),
@@ -34,7 +53,7 @@ const createCard = (userObj) => {
     userFollowers = document.createElement('p'),
     userFollowing = document.createElement('p'),
     userBio = document.createElement('p');
-    
+
 
   card.classList.add('card');
   cardInfo.classList.add('card-info');
@@ -82,14 +101,14 @@ const createCard = (userObj) => {
 
 */
 
-/* Step 1: using axios, send a GET request to the following URL 
+/* Step 1: using axios, send a GET request to the following URL
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
 
-/* Step 2: Inspect and study the data coming back, this is YOUR 
-   github info! You will need to understand the structure of this 
-   data in order to use it to build your component function 
+/* Step 2: Inspect and study the data coming back, this is YOUR
+   github info! You will need to understand the structure of this
+   data in order to use it to build your component function
 
    Skip to Step 3.
 */
@@ -97,16 +116,16 @@ const createCard = (userObj) => {
 // Step 3: Create a function that accepts a single object as its only argument,
 //           Using DOM methods and properties, create a component that will return the following DOM element:
 
-/* Step 4: Pass the data received from Github into your function, 
+/* Step 4: Pass the data received from Github into your function,
            create a new component and add it to the DOM as a child of .cards
 */
 
-/* Step 5: Now that you have your own card getting added to the DOM, either 
-          follow this link in your browser https://api.github.com/users/<Your github name>/followers 
-          , manually find some other users' github handles, or use the list found 
+/* Step 5: Now that you have your own card getting added to the DOM, either
+          follow this link in your browser https://api.github.com/users/<Your github name>/followers
+          , manually find some other users' github handles, or use the list found
           at the bottom of the page. Get at least 5 different Github usernames and add them as
           Individual strings to the friendsArray below.
-          
+
           Using that array, iterate over it, requesting data for each user, creating a new card for each
           user, and adding that card to the DOM.
 */
